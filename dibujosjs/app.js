d79f49f2ab44e5175059856179b31238fb5eda23
Kevin8548 <kevinarroyolopez231@gmail.com>
2024-10-16 13:08:20 -0600

    03 Dibujo Canvas

---
+++ dibujosjs/app.js
@@ -0,0 +1,256 @@
+const lienzo = document.querySelector('#lienzo');
+const ctx = lienzo.getContext('2d');
+
+/**
+ * fillRect (x,y,width, height)
+ * strokeRect(x,y,width, height)
+ * arc(x.y.radio,ini,fin,sentidohorario)
+ */
+
+// ctx.strokeRect(0, 0, 200 ,200);
+// ctx.fillRect(20, 20, 160, 160);
+
+// ctx.fillStyle = 'orange';
+// ctx.beginPath();
+// ctx.arc(100, 100, 60, 0, Math.PI * 2, true);
+// ctx.fill();
+
+// ctx.fillStyle = 'red';
+// ctx.beginPath();
+// ctx.moveTo(80, 90);
+// ctx.lineTo(130, 90);
+// ctx.lineTo(130, 130);
+// ctx.fill();
+
+// CARITA
+
+// ctx.fillStyle = 'black';
+// ctx.beginPath();
+// ctx.arc( 300, 200, 50, 0, Math.PI * 2, true);
+// ctx.fill();
+
+// ctx.fillStyle = 'yellow';
+// ctx.fillRect(270, 180, 20, 5);
+// ctx.fillRect(310, 180, 20, 5);
+
+
+// ctx.beginPath();
+// ctx.arc( 280, 190, 5, 0, Math.PI * 2, true);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc( 320, 190, 5, 0, Math.PI * 2, true);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc( 300, 200, 30, 0, Math.PI, false);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.roundRect(100, 100, 50, 20 ,5)
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.roundRect(280, 135, 50, 20 ,[0,20,0,20]);
+// ctx.fill();
+// ctx.stroke();
+
+
+
+// MIO
+
+ctx.fillStyle = 'Black';
+ctx.beginPath();
+ctx.arc( 295, 148, 50, 0, Math.PI * 2, true);
+ctx.fill();
+ctx.stroke();
+
+
+
+
+ctx.fillStyle = 'yellow';
+// ctx.fillRect(270, 180, 20, 5);
+// ctx.fillRect(310, 180, 20, 5);
+
+
+// ctx.beginPath();
+// ctx.arc( 280, 190, 5, 0, Math.PI * 2, true);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc( 300, 200, 30, 0, Math.PI, false);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.roundRect(100, 100, 50, 20 ,5)
+// ctx.fill();
+
+ctx.beginPath();
+ctx.roundRect(250, 135, 40, 22 ,[5,20,5,20]);
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.roundRect(300, 135, 40, 22 ,[20,5,20,5]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.arc( 320, 145, 5, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.arc( 270, 145, 5, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.fillStyle = 'pink';
+ctx.beginPath();
+ctx.moveTo(295, 170);
+ctx.lineTo(300, 160);
+ctx.lineTo(290, 160);
+ctx.fill();
+
+
+
+
+ctx.fillStyle= 'black';
+ctx.beginPath();
+ctx.roundRect(240, 90, 50, 45 ,[5,60,30,60]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'pink';
+ctx.beginPath();
+ctx.roundRect(250, 100, 21, 21 ,[10,60,10,60]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'black';
+ctx.beginPath();
+ctx.roundRect(259, 108, 20, 20 ,[40,30,30,30]);
+ctx.fill();
+ctx.stroke();
+
+
+
+ctx.fillStyle= 'black';
+ctx.beginPath();
+ctx.roundRect(300, 90, 50, 45 ,[60,5,60,30]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'pink';
+ctx.beginPath();
+ctx.roundRect(319, 100, 21, 21 ,[60,10,60,10]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'black';
+ctx.beginPath();
+ctx.roundRect(311, 109, 20, 20 ,[40,30,30,30]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.roundRect(250, 185, 70, 102 ,[20,5,20,5]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.roundRect(240, 235, 90, 52 ,[20,20,20,20]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.roundRect(240, 260, 10, 90 ,[20,20,20,20]);
+ctx.fill();
+ctx.stroke();
+
+// ctx.fillStyle = 'black';
+// ctx.beginPath();
+// ctx.roundRect(282, 260, 10, 35 ,[20,20,20,20]);
+// ctx.fill();
+// ctx.stroke();
+
+ctx.fillStyle = 'brown';
+ctx.beginPath();
+ctx.roundRect(160, 288, 280, 12 , [20]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'yellow';
+ctx.beginPath();
+ctx.moveTo(50, 360);
+ctx.lineTo(170, 295);
+ctx.lineTo(50, 220);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'yellow';
+ctx.beginPath();
+ctx.moveTo(50, 340);
+ctx.lineTo(170, 295);
+ctx.lineTo(50, 250);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'yellow';
+ctx.beginPath();
+ctx.moveTo(50, 320);
+ctx.lineTo(170, 295);
+ctx.lineTo(50, 260);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'yellow';
+ctx.beginPath();
+ctx.moveTo(50, 300);
+ctx.lineTo(170, 295);
+ctx.lineTo(50, 280);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.roundRect(260, 250, 20, 50 ,[20,20,20,20]);
+ctx.fill();
+ctx.stroke();
+
+
+ctx.fillStyle = 'black';
+ctx.beginPath();
+ctx.roundRect(295, 250, 20, 50 ,[20,20,20,20]);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'white';
+ctx.beginPath();
+ctx.arc( 500, 100, 60, 0, Math.PI * 2, true);
+ctx.fill();
+ctx.stroke();
+
+ctx.fillStyle= 'gray';
+ctx.beginPath();
+ctx.arc( 470, 80, 12, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.fillStyle= 'gray';
+ctx.beginPath();
+ctx.arc( 510, 120, 20, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.fillStyle= 'gray';
+ctx.beginPath();
+ctx.arc( 520, 70, 8, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.fillStyle = 'gray';
+ctx.beginPath();
+ctx.roundRect(160, 288, 11, 12 , [2]);
+ctx.fill();
+ctx.stroke();
